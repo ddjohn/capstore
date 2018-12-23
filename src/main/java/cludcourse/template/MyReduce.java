f08b71d7bdf7eea2f14e278fd6a4a91c0244a66e
David Johansson <ddjohn@gmail.com>
2018-12-23 18:37:59 +0100

    111

---
+++ src/main/java/cludcourse/template/MyReduce.java
@@ -0,0 +1,18 @@
+package cludcourse.template;
+
+import java.io.IOException;
+import org.apache.hadoop.io.*;
+import org.apache.hadoop.mapreduce.Reducer;
+
+public class MyReduce extends Reducer<Text, IntWritable, Text, IntWritable> {
+
+	@Override
+	public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+
+		int sum = 0;
+		for (IntWritable val : values) {
+			sum += val.get();
+		}
+		context.write(key, new IntWritable(sum));
+	}
+}
