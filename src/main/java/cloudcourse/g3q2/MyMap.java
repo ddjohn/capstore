6fb2bdd7f740074e5e3d1f5aa39e8d29d05cd410
David Johansson <ddjohn@gmail.com>
2018-12-28 15:47:58 +0100

    vvv

--- src/main/java/cloudcourse/g3q2/MyMap.java
+++ src/main/java/cloudcourse/g3q2/MyMap.java
@@ -1,4 +1,4 @@
-package cloudcourse.g2q4;
+package cloudcourse.g3q2;
 
 import java.io.IOException;
 import org.apache.hadoop.io.*;
@@ -21,15 +21,14 @@ public class MyMap extends Mapper<Object, Text, Text, FloatWritable> {
 		if(tokens.length > DataSet.ARRDELAY && 
 				tokens[DataSet.ORIGIN].isEmpty() == false && 
 				tokens[DataSet.DEST].isEmpty() == false && 
-				tokens[DataSet.FLIGHTDATE].isEmpty() == false && 
-				tokens[DataSet.ARRDELAY].isEmpty() == false) {
-
-			combo.set(tokens[DataSet.ORIGIN] + "_" + tokens[DataSet.DEST] + "_" + DataSet.FLIGHTDATE);
+				tokens[DataSet.FLIGHTDATE].isEmpty() == false) {
+			
+			combo.set(tokens[DataSet.ORIGIN] + "_" + tokens[DataSet.DEST] + "_" + tokens[DataSet.FLIGHTDATE]);
 			delay.set(Float.parseFloat(tokens[DataSet.ARRDELAY]));
 			context.write(combo, delay);
 		} 
 		else {
-			System.out.println("Discarding: " + line);
+			//System.out.println("Discarding: " + line);
 			DataSet.discarded++;
 		}
 	}
--- src/main/java/cloudcourse/globals/DataSet.java
+++ src/main/java/cloudcourse/globals/DataSet.java
@@ -12,19 +12,7 @@ public class DataSet {
 	public static final int DEPTIME       = 6;
 	public static final int DEPDELAY      = 7;
 	public static final int ARRDELAY      = 8;
-	/*
-	public static final int AIRLINEID = 0;
-	public static final int UNIQUECARRIER = 2;
-	public static final int DAYOFWEEK = 4;
-	public static final int DEST = 5;
-	public static final int ORIGIN = 6;
-	public static final int DEPDELAY = 7;
-	public static final int ARRDELAY = 9;
-	 */
+
 	public static int discarded = 0;
 
 }
-/* 
-	AIRLINEID, CARRIER, UNIQUECARRIER, FLIGHTNUM, DAYOFWEEK, DEST, ORIGIN, DEPDELAY, DEPDEL15, ARRDELAY, ARRDEL15
-	(19805,AA,AA,227,5,DFW,MSY,-1.0,0.0,-9.0,0.0)                                  
- */
