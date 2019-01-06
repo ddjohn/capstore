2c11d2863ea4f9ef7623f0035a4962db45c808b3
David Johansson <ddjohn@gmail.com>
2019-01-06 21:01:39 +0100

    ghi

---
+++ src/main/java/spark/G2Q4Main.java
@@ -0,0 +1,52 @@
+package spark;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.apache.spark.api.java.Optional;
+import scala.Tuple2;
+import cloudcourse.globals.DataSet;
+
+public class G2Q4Main {
+
+	public static final void main(String[] args) throws InterruptedException {
+		MyContext ctx = new MyContext();
+
+		ctx.createStream("cloudcourse")
+
+		.flatMapToPair(x -> {
+			String[] tokens =  x.value().substring(1, x.value().length() - 1).split(",");
+
+			List<Tuple2<String, Float>> list = new ArrayList<Tuple2<String, Float>>();
+			if(tokens.length > DataSet.ARRDELAY && 
+					tokens[DataSet.ORIGIN].isEmpty() == false && 
+					tokens[DataSet.DEST].isEmpty() == false && 
+					tokens[DataSet.ARRDELAY].isEmpty() == false) {
+
+				list.add(new Tuple2<String, Float>(tokens[DataSet.ORIGIN] + "_" + tokens[DataSet.DEST], Float.parseFloat(tokens[DataSet.ARRDELAY])));
+			}
+			return list.iterator();
+		})
+
+		// Remember the keys 
+		.updateStateByKey((nums, current) -> {
+
+			Average average = current.or(new Average());
+			for(float i : nums) {
+				average.count++;
+				average.sum += i;
+			}
+			return Optional.of(average);
+		})
+
+		// Sort by swapping values to keys and back
+		.mapToPair(x -> x.swap())
+		.transformToPair(x -> x.sortByKey(true))
+		.mapToPair(x -> x.swap())
+
+		// Print top 10
+		.print(10000);
+
+		ctx.run();
+		ctx.close();
+	}
+}
