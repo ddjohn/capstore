11353937a78e5a9545d8d7348ecad0e61e84e182
David Johansson <ddjohn@gmail.com>
2019-01-13 19:44:25 +0100

    890

--- storm/producer.sh
+++ storm/producer.sh
@@ -2,7 +2,7 @@
 
 TOPIC=cloudcourse
 
-hdfs dfs -cat /input_big | head -10
+hdfs dfs -cat /input | head -10
 
-hdfs dfs -cat /input_big | /opt/kafka/bin/kafka-console-producer.sh --broker-list localhost:9092 --topic ${TOPIC}
+hdfs dfs -cat /input | /opt/kafka/bin/kafka-console-producer.sh --broker-list localhost:9092 --topic ${TOPIC}
 
