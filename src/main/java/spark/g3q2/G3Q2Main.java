04af216d0901356e722f1526c64e6b65c2d4c90f
David Johansson <ddjohn@gmail.com>
2019-01-15 10:14:23 +0100

    klm

--- src/main/java/spark/g3q2/G3Q2Main.java
+++ src/main/java/spark/g3q2/G3Q2Main.java
@@ -4,22 +4,14 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-
 import org.apache.spark.streaming.api.java.JavaDStream;
-import org.apache.spark.streaming.api.java.JavaPairDStream;
-
 import com.datastax.spark.connector.japi.CassandraJavaUtil;
-
 import mapreduce.globals.DataSet;
-import scala.Tuple2;
-import spark.g2q4.G2Q4Database;
 import spark.globals.MyContext;
 
 public class G3Q2Main implements Serializable {
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
+
 	private String[] FILTER_ORIGIN = {"BOS", "PHX", "DFW", "LAX"};
 	private String[] FILTER_LEG    = {"ATL", "JFK", "STL", "MIA"};
 	private String[] FILTER_DEST   = {"LAX", "MSP", "ORD", "LAX"};
@@ -30,40 +22,35 @@ public class G3Q2Main implements Serializable {
 
 		JavaDStream<TomsFlight> stream = ctx.createStream("cloudcourse")
 
-		.flatMap(x -> {
-			List<TomsFlight> list = new ArrayList<TomsFlight>();
+				.flatMap(x -> {
+					List<TomsFlight> list = new ArrayList<TomsFlight>();
 
-			String[] tokens = x.value().substring(1, x.value().length() - 1).split(",");
+					String[] tokens = x.value().substring(1, x.value().length() - 1).split(",");
 
-			if(tokens.length > DataSet.ARRDELAY) {
-				try {
-					list.add(new TomsFlight(
-							tokens[DataSet.ORIGIN], 
-							tokens[DataSet.DEST], 
-							tokens[DataSet.FLIGHTDATE],
-							tokens[DataSet.DEPTIME],
-							tokens[DataSet.UNIQUECARRIER] + " " + tokens[DataSet.FLIGHTNUM],
-							Float.parseFloat(tokens[DataSet.ARRDELAY])));
-				}
-				catch(Exception e) {
-					System.out.println("e: " + e);
-				}
-			}
-			return list.iterator();
-		})
+					if(tokens.length > DataSet.ARRDELAY) {
+						list.add(new TomsFlight(
+								tokens[DataSet.ORIGIN], 
+								tokens[DataSet.DEST], 
+								tokens[DataSet.FLIGHTDATE],
+								tokens[DataSet.DEPTIME],
+								tokens[DataSet.UNIQUECARRIER] + " " + tokens[DataSet.FLIGHTNUM],
+								Float.parseFloat(tokens[DataSet.ARRDELAY])));
+					}
+					return list.iterator();
+				})
 
-		// Limit to flights from 2008
-		.filter(x -> x.depDate.startsWith("2008"))
+				// Limit to flights from 2008
+				.filter(x -> x.depDate.startsWith("2008"))
 
-		.filter(x -> {
-			if("1200".compareTo(x.depTime) >= 0 && Arrays.asList(FILTER_ORIGIN).contains(x.origin) && Arrays.asList(FILTER_LEG).contains(x.dest) ) {
-				return true;
-			}
-			if("1200".compareTo(x.depTime) <= 0 && Arrays.asList(FILTER_LEG).contains(x.origin) && Arrays.asList(FILTER_DEST).contains(x.dest) ) {
-				return true;
-			}
-			return false;
-		});
+				.filter(x -> {
+					if("1200".compareTo(x.depTime) >= 0 && Arrays.asList(FILTER_ORIGIN).contains(x.origin) && Arrays.asList(FILTER_LEG).contains(x.dest) ) {
+						return true;
+					}
+					if("1200".compareTo(x.depTime) <= 0 && Arrays.asList(FILTER_LEG).contains(x.origin) && Arrays.asList(FILTER_DEST).contains(x.dest) ) {
+						return true;
+					}
+					return false;
+				});
 
 		// Save to Cassandra
 		stream
